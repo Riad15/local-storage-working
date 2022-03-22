47cdf8d45a603ba7556149df9913e9cc5844c468
asif riad <asifriad15@gmail.com>
2022-03-22 13:08:07 +0600

    working the local storage and save the data

--- src/compnents/Storage/Storage.js
+++ src/compnents/Storage/Storage.js
@@ -1,12 +1,26 @@
 // use local storage to store data 
 const addToData = (id) => {
-    const quantity = localStorage.getItem(id);
+    let person;
+
+    // get the key "employee" from local storage
+    const store = localStorage.getItem('Employee');
+    if (store) {
+        person = JSON.parse(store);
+    } else {
+        person = {};
+    }
+
+    // add to quantity
+    const quantity = person[id];
     if (quantity) {
         const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
+        person[id] = newQuantity;
+        // localStorage.setItem(id, newQuantity);
     } else {
-        localStorage.setItem(id, 1);
+        person[id] = 1;
+        // localStorage.setItem(id, 1);
     }
+    localStorage.setItem('Employee', JSON.stringify(person));
 
 }
 export { addToData }
