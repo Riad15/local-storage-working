cfcfbcb69850d984f28066012e6070caf3b680fc
asif riad <asifriad15@gmail.com>
2022-03-22 12:24:43 +0600

    local storage item store add functionality

--- src/compnents/Person/Person.js
+++ src/compnents/Person/Person.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { addToData } from '../Storage/Storage';
 import './Person.css'
 
 const Person = (props) => {
-    const { name, age, email, balance } = props.person;
+    const { id, name, age, email, balance } = props.person;
+    const moreInfo = () => {
+        addToData(id);
+    }
     return (
         <div className='person'>
             <h3>name: {name} </h3>
             <h4>balance: {balance}</h4>
             <p> email: {email} </p>
-            <button className='btn'>more information</button>
+            <button className='btn' onClick={() => { moreInfo(id) }}>more information</button>
         </div>
     );
 };
